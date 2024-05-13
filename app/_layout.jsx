ba42c72a85c47cdfac7f8b40c1af26d32d9cac0c
Andry Ariadi <andryariadi23@gmail.com>
2024-05-13 16:35:40 +0700

    install and setup nativewind

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,11 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Slot, Stack } from "expo-router";
+import { Stack } from "expo-router";
 
 const RootLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: true }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 };
--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Yoo bre</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "white" }}>
+      <Text style={{ fontSize: 20 }}>Yoo bre</Text>
       <StatusBar style="auto" />
+      <Link href="/profile">Go to Profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#3C5B6F",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
