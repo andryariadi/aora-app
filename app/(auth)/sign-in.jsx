27fc6199fc6e8bd6a2ef9591c3f1ff2de1f0511b
Andry Ariadi <andryariadi23@gmail.com>
2024-05-14 20:56:17 +0700

    add form field component

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView className="bg-rose-500" contentContainerStyle={{ height: "100%" }}>
+        <View className="bg-green-600 w-full h-full justify-start px-4 my-5">
+          <Image source={images.logo} resizeMode="contain" className="w-[115px] h-[35px]" />
+          <Text className="text-2xl text-white font-psemibold mt-8">Log in to Aora</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
