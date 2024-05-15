62a6b93df67bae0f76f9601d5a74498a7d2f359a
Andry Ariadi <andryariadi23@gmail.com>
2024-05-15 14:26:32 +0700

    setup appwrite config

---
+++ libs/appwrite.js
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  platform: "com.andryariadi.aora",
+  projectId: "66445b33002d07413109",
+  databaseId: "66445e6f00230fb6097c",
+  userCollectionId: "66445f0600336cd1b2ea",
+  videoCollectionId: "66445f540039544484ed",
+  storageId: "664462b4001df4e8d318",
+};
