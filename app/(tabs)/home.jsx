7547b673215f9208260b90b5caae423aef8dccf1
Andry Ariadi <andryariadi23@gmail.com>
2024-05-16 21:10:02 +0700

    styling home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
               </View>
             </View>
 
-            <SearchInput />
+            <SearchInput keyboardType="Search..." />
           </View>
         )}
       />
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -7,7 +7,7 @@ const SearchInput = ({ title, value, handleChangeInput, otherStyles, keyboardTyp
 
   return (
     <View className="w-full h-16 bg-black-100 px-4 border-2 border-black-200 rounded-2xl focus:border-secondary flex-row items-center space-x-4">
-      <TextInput className="flex-1 w-full text-white font-pregular" placeholderTextColor="#7b7b8b" value={value} onChangeText={handleChangeInput} secureTextEntry={title === "Password" && !showPassword} />
+      <TextInput className="flex-1 w-full text-white font-pregular" placeholder={keyboardType} placeholderTextColor="#7b7b8b" value={value} onChangeText={handleChangeInput} secureTextEntry={title === "Password" && !showPassword} />
 
       <TouchableOpacity>
         <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
