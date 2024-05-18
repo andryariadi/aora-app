bb8c985354b25898ff307b3a0a6f8c650059d6dc
Andry Ariadi <andryariadi23@gmail.com>
2024-05-18 12:58:56 +0700

    add useAppwrite custom hook

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -5,22 +5,27 @@ import SearchInput from "../../components/SearchInput";
 import TrendingVideo from "../../components/TrendingVideo";
 import EmptyState from "../../components/EmptyState";
 import { useState } from "react";
+import useAppwrite from "../../libs/useAppwrite";
+import { getAllPosts } from "../../libs/appwrite";
 
 const Home = () => {
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
   const [refreshing, setreFreshing] = useState(false);
 
   const onRefresh = async () => {
     setreFreshing(true);
+    await refetch();
     setreFreshing(false);
   };
 
+  console.log(posts, "<----dihome");
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList //alasan pake flatlist karena scrollview tidak bisa menggunakan scroll secara horizontal dan vertikal secara bersamaan
         // className="bg-blue-600 h-full"
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        data={posts}
         keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => <Text className="bg-amber-500 text-3xl text-white">{item.id}</Text>}
+        renderItem={({ item }) => <Text className="bg-amber-500 text-3xl text-white">{item.title}</Text>}
         ListHeaderComponent={() => (
           <View className="bg-rose-500 my-6 px-4 space-y-6">
             <View className="bg-gray-200 flex-row justify-between items-center mb-6">
