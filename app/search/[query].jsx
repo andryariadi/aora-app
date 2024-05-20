3d75603880b89461cc05ab05ed491a571fcf00b7
Andry Ariadi <andryariadi23@gmail.com>
2024-05-20 14:10:35 +0700

    setup search fiture

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,14 +1,41 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import { View, Text, FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import SearchInput from "../../components/SearchInput";
+import EmptyState from "../../components/EmptyState";
+import { useEffect } from "react";
+import useAppwrite from "../../libs/useAppwrite";
+import { searchPosts } from "../../libs/appwrite";
+import VideoCard from "../../components/VideoCard";
 import { useLocalSearchParams } from "expo-router";
 
 const Search = () => {
   const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
 
-  console.log(query, "<---diquerypage");
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
+  console.log(query, posts, "<----diquerypage");
+
   return (
     <SafeAreaView className="bg-primary h-full">
-      <Text className="text-white">{query}</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="bg-rose-500 my-6 px-4">
+            <Text className="font-pmedium text-sm text-gray-100">Search Results</Text>
+            <Text className="text-2xl text-white font-psemibold">{query}</Text>
+
+            <View className="mt-6 mb-8">
+              <SearchInput initialQuery={query} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => <EmptyState title="No Videos Found" subtitle="No Videos Found with that query!" />}
+      />
     </SafeAreaView>
   );
 };
