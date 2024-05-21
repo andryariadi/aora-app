1a9cd491a037a11a84c14affd43e6fab77d28a3e
Andry Ariadi <andryariadi23@gmail.com>
2024-05-21 14:04:25 +0700

    styling profile page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -32,7 +32,13 @@ const Profile = () => {
               <Image source={{ uri: user?.avatar }} className="w-[90%] h-[90%] rounded-lg" resizeMode="cover" />
             </View>
 
-            <InfoBox />
+            <InfoBox title={user?.username} containerStyles="mt-3" titleStyles="text-lg" />
+
+            <View className="mt-5 flex-row">
+              <InfoBox title={posts?.length || 0} subTitle="Posts" containerStyles="mr-10" titleStyles="text-xl" />
+
+              <InfoBox title="1.2k" subTitle="Followers" titleStyles="text-xl" />
+            </View>
           </View>
         )}
         ListEmptyComponent={() => <EmptyState title="No Videos Found" subtitle="No Videos Found with that query!" />}
--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native";
 
-const InfoBox = () => {
+const InfoBox = ({ title, subTitle, containerStyles, titleStyles }) => {
   return (
-    <View>
-      <Text>InfoBox</Text>
+    <View className={`bg-amber-500 ${containerStyles}`}>
+      <Text className={`${titleStyles} text-white text-center font-psemibold`}>{title}</Text>
+      <Text className="text-sm text-gray-100 text-center font-pregular">{subTitle}</Text>
     </View>
   );
 };
